cd11a09c105a774a23fb32c34a72be40fbd4aab6
Cristian Misael Lemus <87676318+CrisLemus21@users.noreply.github.com>
2024-06-06 19:43:08 -0600

    final

--- placas/data/dbConexion.js
+++ placas/data/dbConexion.js
@@ -1,21 +1,21 @@
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'dbplacas'
-})
+require('dotenv').config();
+const mysql = require('mysql');
 
-connection.connect(
-(err)=>{
-    if (err){
-        console.log("Conexion establecida");
-    }
-    
-    else{
-        console.log("No se pudo conectar")
-    }
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
 function buscador(){
     connection.query("SELECT * FROM tbplaca WHERE numPlaca = 18183", function(err, resultados){
         console.log(resultados);
